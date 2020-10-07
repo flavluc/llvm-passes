97b7b60dd89fa27a4fa24faad03ce8785bc7b652
Flávio Lúcio <flaviolcj@protonmail.com>
2020-10-07 15:55:03 -0300

    temp

---
+++ dead-code-elimination/build/CMakeFiles/DeadCodeElimination.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mab/Desktop/college/aep/tps/dead-code-elimination/DeadCodeElimination.cpp" "/home/mab/Desktop/college/aep/tps/dead-code-elimination/build/CMakeFiles/DeadCodeElimination.dir/DeadCodeElimination.cpp.o"
+  "/home/mab/Desktop/college/aep/tps/dead-code-elimination/lib/RangeAnalysis.cpp" "/home/mab/Desktop/college/aep/tps/dead-code-elimination/build/CMakeFiles/DeadCodeElimination.dir/lib/RangeAnalysis.cpp.o"
+  "/home/mab/Desktop/college/aep/tps/dead-code-elimination/lib/vSSA.cpp" "/home/mab/Desktop/college/aep/tps/dead-code-elimination/build/CMakeFiles/DeadCodeElimination.dir/lib/vSSA.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DeadCodeElimination_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ dead-code-elimination/build/CMakeFiles/DeadCodeElimination.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeadCodeElimination.dir/DeadCodeElimination.cpp.o"
+  "CMakeFiles/DeadCodeElimination.dir/lib/RangeAnalysis.cpp.o"
+  "CMakeFiles/DeadCodeElimination.dir/lib/vSSA.cpp.o"
+  "libDeadCodeElimination.pdb"
+  "libDeadCodeElimination.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeadCodeElimination.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
